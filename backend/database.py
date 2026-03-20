61fb70b2d1dc58d259897bafa577146b66699b91
ananth-kn <engrananthn@email.com>
2026-03-20 09:07:36 +0000

    fix ssl connection for neon db

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,16 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import settings
 
-engine = create_async_engine(settings.sqlalchemy_database_url)
+import ssl
 
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
+
+engine = create_async_engine(
+    settings.sqlalchemy_database_url,
+    connect_args={"ssl": ssl_context}
+)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
