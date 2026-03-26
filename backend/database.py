ca1787e327b995d636b00c08f99841927cdf09d3
ananth-kn <engr.ananthn@gmail.com>
2026-03-26 11:07:18 +0000

    fix: check local bool for defining engine

--- backend/database.py
+++ backend/database.py
@@ -4,17 +4,19 @@ from config import settings
 
 import ssl
 
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-
-engine = create_async_engine(
-    settings.sqlalchemy_database_url,
-    connect_args={"ssl": ssl_context},
-    pool_pre_ping=True,
-    pool_recycle=300,
-    pool_size=5,
-)
+if settings.local:
+    engine = create_async_engine(settings.sqlalchemy_database_url)
+else:
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+    engine = create_async_engine(
+        settings.sqlalchemy_database_url,
+        connect_args={"ssl": ssl_context},
+        pool_pre_ping=True,
+        pool_recycle=300,
+        pool_size=5,
+    )
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 Base = declarative_base()
