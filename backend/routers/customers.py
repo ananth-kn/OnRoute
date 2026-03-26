fd46bf41d28d0289df87ae6f9f541301d9a802ac
ananth-kn <engr.ananthn@gmail.com>
2026-03-26 11:01:29 +0000

    fix: return 404 for invalid customer_token

--- backend/routers/customers.py
+++ backend/routers/customers.py
@@ -1,31 +1,29 @@
-from fastapi import APIRouter, HTTPException, Form, File, UploadFile, Depends, WebSocket, WebSocketDisconnect, Request
-import redis.asyncio as redis
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import IntegrityError, DataError
 from sqlalchemy import select
 from database import get_db
-from oauth2 import get_current_tenant
-import models, schemas, utils, oauth2
-from typing import List
-from uuid import UUID
-from config import limiter, r
+import models, schemas
+from config import limiter
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix="/customers")
 templates = Jinja2Templates(directory="../frontend")
 
-@router.get("/{customer_token}", status_code=200)
-async def set_customer_location(request: Request, customer_token: str):
+@router.get("/{customer_token}")
+async def set_customer_location(request: Request, customer_token: str, db: AsyncSession= Depends(get_db)):
+    delivery = (await db.execute(select(models.Delivery).where(models.Delivery.customer_token == customer_token))).scalars().first()
+    if not delivery:
+            return {"message": "Page not found"}
     return templates.TemplateResponse("customer.html", {"request":request, "customer_token": customer_token})
 
 
-@router.patch("/location/{customer_token}", status_code=200)
+@router.patch("/location/{customer_token}")
 @limiter.limit("10/minute")
 async def add_customer_location(request: Request, data: schemas.CustomerLocation, customer_token: str, db: AsyncSession= Depends(get_db)):
     delivery = (await db.execute(select(models.Delivery).where(models.Delivery.customer_token == customer_token))).scalars().first()
     delivery.customer_lat = data.lat
     delivery.customer_lng = data.lng
     await db.commit()
-    return templates.TemplateResponse("customer.html", {"request":request, "customer_token": customer_token})
-
+    print("done")
+    return {"message": "location updated"}
 
